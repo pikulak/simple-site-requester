cc1721b9e48f508bcd64c06943619a3cbc9ef303
Kacper Pikulski <kacper.p@webinterpret.com>
2017-09-26 13:12:39 +0200

    Edited entrypoint and added it to compose config yml

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -eo pipefail
+
+# check requirements.txt everytime container stars,
+# it allows you to dynamically edit requirements.txt without rebuilding image
+pip install -r requirements.txt
 exec "$@"
